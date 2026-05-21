4f48731600d3bc6238fb0c37d5be97abb23f5e01
chaymaemoukani-maker <chiamaa.moukani12@gmail.com>
2026-05-21 16:52:51 +0100

    feat: display only available books with dynamic cards and read button

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ async function getBooks() {
 displayBooks(availableBooks);
 }
 
+function displayBooks(books) {
+
+  booksContainer.innerHTML = "";
+
+  books.forEach(book => {
+
+    booksContainer.innerHTML += `
+
+      <div class="card">
+
+        <div class="img-container">
+
+          <img src="${book.couverture}" alt="${book.titre}">
+
+          <div class="overlay">
+
+            <h3>${book.titre}</h3>
+
+            <button class="read-btn" data-id="${book.id}">
+              Lire
+            </button>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    `;
+  });
+
+  
