8abc8dd16fe99cbeae525349b786472d8796c050
chaymaemoukani-maker <chiamaa.moukani12@gmail.com>
2026-05-22 15:27:47 +0100

    initialize admin Dom elements and editig state variables

---
+++ admin.js
@@ -0,0 +1,7 @@
+const table        = document.getElementById("booksTable");
+const formContainer = document.getElementById("formContainer");
+const showFormBtn  = document.getElementById("showFormBtn");
+const formTitle    = document.getElementById("formTitle");
+const submitBtn    = document.getElementById("submitBtn");
+
+let editingId = null;
